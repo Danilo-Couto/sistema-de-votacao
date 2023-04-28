cf793485b22964d67d7f55d79a39e05ad13ba860
Danilo Couto <couto.danilo95@gmail.com>
2023-04-28 18:27:00 -0300

    add class Pessoa Eleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,19 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    super(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
