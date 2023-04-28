a9a63ab9094f41c29a63ffcf3a9250793c3b3fe5
Danilo Couto <couto.danilo95@gmail.com>
2023-04-28 18:25:50 -0300

    add class Pessoa

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
@@ -1,5 +1,17 @@
 package com.trybe.acc.java.sistemadevotacao;
 
 public class Pessoa {
+  private String nome;
 
+  public Pessoa(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
